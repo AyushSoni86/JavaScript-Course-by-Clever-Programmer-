101e81a028215bab3fc5ddd4f36f727e66ba4e2e
AyushSoni86 <ayusoni86@gmail.com>
2022-10-07 19:45:14 +0530

    learned about arrow functions

--- yourPlayground.js
+++ yourPlayground.js
@@ -3,23 +3,55 @@
 // fruit = prompt("what is your favourite fruit ?");
 // console.log(fruit);
 
-/*
-Automatic tip calculator 
-food = prompt("How much was the food ?");
-tipPercentage = prompt("Tip %") / 100;
-tipAmount = food * tipPercentage;
-alert("The tip amount is " + tipAmount);
-
-console.log( "The total amount is ", Number(tipAmount) + Number(food));
-*/
-
 // console.log(Math.floor(Math.random() * 30));
 
-let wheather = prompt('What is the weather ?');
+// Automatic tip calculator 
+function tipCalculate() {
+    food = prompt("How much was the food ?");
+    tipPercentage = prompt("Tip %") / 100;
+    tipAmount = food * tipPercentage;
+    alert("The tip amount is " + tipAmount);
 
-if (wheather == "rain") {
-    console.log("Grab your umbrella");
+    console.log("The total amount is ", Number(tipAmount) + Number(food));
 }
-else {
-    console.log("Wear your sunglasses")
+
+// simple wheater practice code 
+function wheatherApp() {
+    let wheather = prompt('What is the weather ?');
+    if (wheather == "rain") {
+        console.log("Grab your umbrella");
+    }
+    else {
+        console.log("Wear your sunglasses")
+    }
+}
+
+// this function prints my name
+function greeting(name) {
+    console.log(`Hey ${name}, how are you?`)
+}
+// greeting("Ayush")
+
+function sum(a, b) {
+    return a + b;
+}
+// Basic tip calculator 
+function tipCalculator(food, tip) {
+    const tipPercentage = tip / 100;
+    const tipAmount = tipPercentage * food;
+    const total = sum(tipAmount, food);
+    return total;
+}
+const tipCalculatorArrow = (food, tip)=> {
+    const tipPercentage = tip / 100;
+    const tipAmount = tipPercentage * food;
+    const total = sum(tipAmount, food);
+    return total;
+}
+console.log("The total amount pof food  is ", tipCalculatorArrow(10000, 5));
+
+
+// Arrow functions 
+const sumArrow = (a, b) => {
+    return a + b;
 }
