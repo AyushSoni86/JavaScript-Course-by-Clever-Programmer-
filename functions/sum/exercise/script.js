b5a63b6a47fdf9268d8fad28a5b2d932eae0f1e5
AyushSoni86 <ayusoni86@gmail.com>
2022-10-07 19:55:05 +0530

    completed the functions exercise by Qazi

--- functions/sum/exercise/script.js
+++ functions/sum/exercise/script.js
@@ -8,23 +8,46 @@
   ES6 Syntax (Arrow function): const add = () => {}
 */
 
-function add(){
-  //Add function here
+function add(a, b) {
+  return a + b;
 }
 
-function sub(){
-  //Subtract function here
+function sub(a, b) {
+  return a - b;
 }
 
-function div(){
-  //Divide function here
+function div(a, b) {
+  if(b==0){
+    console.log("cannot divide by 0")
+    return;
+  }
+  return a / b;
 }
 
-function mul(){
-  //Multiply function here
+function mul(a, b) {
+  return a * b;
 }
+
+// ES6 version of functions
+
+const addArrow = (a, b) => a + b;
+
+
+const subArrow = (a, b) => a - b;
+
+
+const divArrow = (a, b) => a / b;
+
+
+const mulArrow = (a, b) => a * b;
+
 
 console.log('hello from the SUM exercise')
 /* 
   TODO: create a function that console logs the result of any of the above operations.
 */
+
+console.log(addArrow(20, 50));
+console.log(subArrow(20, 50));
+console.log(mulArrow(20, 50));
+console.log(div(20, 0));
