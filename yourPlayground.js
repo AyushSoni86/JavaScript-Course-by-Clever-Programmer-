b7bd5e676598dcd1d81e0b4f3b7a4062c4c09652
AyushSoni86 <ayusoni86@gmail.com>
2022-10-07 19:17:16 +0530

    Getting Started

---
+++ yourPlayground.js
@@ -0,0 +1,25 @@
+// console.log("hello world");
+// user input 
+// fruit = prompt("what is your favourite fruit ?");
+// console.log(fruit);
+
+/*
+Automatic tip calculator 
+food = prompt("How much was the food ?");
+tipPercentage = prompt("Tip %") / 100;
+tipAmount = food * tipPercentage;
+alert("The tip amount is " + tipAmount);
+
+console.log( "The total amount is ", Number(tipAmount) + Number(food));
+*/
+
+// console.log(Math.floor(Math.random() * 30));
+
+let wheather = prompt('What is the weather ?');
+
+if (wheather == "rain") {
+    console.log("Grab your umbrella");
+}
+else {
+    console.log("Wear your sunglasses")
+}
