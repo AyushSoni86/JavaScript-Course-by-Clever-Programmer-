2a388642b449c97d44a60a9c78707dade0d127df
AyushSoni86 <ayusoni86@gmail.com>
2022-10-09 22:05:37 +0530

    continued loops

--- yourPlayground.js
+++ yourPlayground.js
@@ -1,62 +1,60 @@
 // console.log("hello world");
-// user input 
+// user input
 // fruit = prompt("what is your favourite fruit ?");
 // console.log(fruit);
 
 // console.log(Math.floor(Math.random() * 30));
 
-// Automatic tip calculator 
+// Automatic tip calculator
 function tipCalculate() {
-    food = prompt("How much was the food ?");
-    tipPercentage = prompt("Tip %") / 100;
-    tipAmount = food * tipPercentage;
-    alert("The tip amount is " + tipAmount);
+  food = prompt("How much was the food ?");
+  tipPercentage = prompt("Tip %") / 100;
+  tipAmount = food * tipPercentage;
+  alert("The tip amount is " + tipAmount);
 
-    console.log("The total amount is ", Number(tipAmount) + Number(food));
+  console.log("The total amount is ", Number(tipAmount) + Number(food));
 }
 
-// simple wheater practice code 
+// simple wheater practice code
 function wheatherApp() {
-    let wheather = prompt('What is the weather ?');
-    if (wheather == "rain") {
-        console.log("Grab your umbrella");
-    }
-    else {
-        console.log("Wear your sunglasses")
-    }
+  let wheather = prompt("What is the weather ?");
+  if (wheather == "rain") {
+    console.log("Grab your umbrella");
+  } else {
+    console.log("Wear your sunglasses");
+  }
 }
 
 // this function prints my name
 function greeting(name) {
-    console.log(`Hey ${name}, how are you?`)
+  console.log(`Hey ${name}, how are you?`);
 }
 // greeting("Ayush")
 
 function sum(a, b) {
-    return a + b;
+  return a + b;
 }
-// Basic tip calculator 
+// Basic tip calculator
 function tipCalculator(food, tip) {
-    const tipPercentage = tip / 100;
-    const tipAmount = tipPercentage * food;
-    const total = sum(tipAmount, food);
-    return total;
+  const tipPercentage = tip / 100;
+  const tipAmount = tipPercentage * food;
+  const total = sum(tipAmount, food);
+  return total;
 }
 const tipCalculatorArrow = (food, tip) => {
-    const tipPercentage = tip / 100;
-    const tipAmount = tipPercentage * food;
-    const total = sum(tipAmount, food);
-    return total;
-}
+  const tipPercentage = tip / 100;
+  const tipAmount = tipPercentage * food;
+  const total = sum(tipAmount, food);
+  return total;
+};
 // console.log("The total amount pof food  is ", tipCalculatorArrow(10000, 5));
 
-
-// Arrow functions 
+// Arrow functions
 const sumArrow = (a, b) => {
-    return a + b;
-}
+  return a + b;
+};
 
-// Arrays 
+// Arrays
 
 // let groceries= ['apple', 'banana', 'pear', 'orange','berry'];
 // console.log(groceries);
@@ -66,16 +64,14 @@ const sumArrow = (a, b) => {
 // slicedArray = groceries.slice(1,3);
 // console.log(slicedArray);
 
-
-
 // Objects
-// dot notation and bracket notation in objects 
+// dot notation and bracket notation in objects
 const person = {
-    name: "Ayush",
-    shirt: 'black',
-    email: 'ayusoni86@gmail.com'
-}
-person.phone = '9588422464'
+  name: "Ayush",
+  shirt: "black",
+  email: "ayusoni86@gmail.com",
+};
+person.phone = "9588422464";
 
 // console.log(person.name)
 // console.log(person.shirt)
@@ -88,26 +84,44 @@ person.phone = '9588422464'
 // console.log(person)
 
 const introducer = (name, shirt, mobile) => {
-    const person = {
-        name: name,
-        shirt: shirt,
-        mobile: mobile,
-        assets: 100000,
-        liability: 50000,
-        networth: function () {
-            return this.assets - this.liability;
-        }
-    };
-    return `Hi my name is ${person.name} and the color of my shirt is ${person.shirt} and I have ${person.mobile} and my nte worth is ${person.networth()} USD `;
-}
+  const person = {
+    name: name,
+    shirt: shirt,
+    mobile: mobile,
+    assets: 100000,
+    liability: 50000,
+    networth: function () {
+      return this.assets - this.liability;
+    },
+  };
+  return `Hi my name is ${person.name} and the color of my shirt is ${person.shirt}
+   and I have ${person.mobile} and my nte worth is ${person.networth()} USD `;
+};
 // console.log(introducer("Ayush", "Blue", "iPhone 13"));
 
-
-// for loop in javascript 
-const fruits = ['apple', 'banana', 'pear', 'orange', 'berry', 'banana', 'pear', 'orange', 'berry', 'banana', 'pear', 'orange', 'berry', 'banana', 'pear', 'orange', 'berry'];
+// for loop in javascript
+const fruits = [
+  "apple",
+  "banana",
+  "pear",
+  "orange",
+  "berry",
+  "banana",
+  "pear",
+  "orange",
+  "berry",
+  "banana",
+  "pear",
+  "orange",
+  "berry",
+  "banana",
+  "pear",
+  "orange",
+  "berry",
+];
 
 // for (let index = 0; index < fruits.length; index++) {
-//     console.log(index , fruits[index]); 
+//     console.log(index , fruits[index]);
 // }
 
 // for (const fruit of fruits) {
@@ -116,7 +130,7 @@ const fruits = ['apple', 'banana', 'pear', 'orange', 'berry', 'banana', 'pear', 
 
 const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // for (let i = 0; i < number.length; i++) {
-//     console.log(number[i]);   
+//     console.log(number[i]);
 // }
 // for (nums of number){
 //     console.log(nums);
@@ -128,34 +142,57 @@ const number = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 // }
 // console.log(res);
 
-// function to double the array elements 
+// function to double the array elements
 const double = (number) => {
-    let res = [];
-    for (nums of number) {
-        res.push(nums * 2);
-    }
-    return res;
-}
+  let res = [];
+  for (nums of number) {
+    res.push(nums * 2);
+  }
+  return res;
+};
 
 // console.log(double([1,2,3,4,5,8,9]));
-// function to square the array elements 
-const square = (number) =>{
-    let result = [];
-    for(const num of number){
-        result.push(num*num);
-    }
-    return result;
-}
+// function to square the array elements
+const square = (number) => {
+  let result = [];
+  for (const num of number) {
+    result.push(num * num);
+  }
+  return result;
+};
 
 // console.log(square([2,3,4,5,6]));
 
 //letter counter using loops
-const letterCounter = () =>{
-    const phrase = "hey, can you come to grocery store with me";
-    let cnt = 0;
-    for(letter in phrase){
-        cnt++;
+const letterCounter = () => {
+  const phrase = "hey, can you come to grocery store with me";
+  let cnt = 0;
+  for (letter in phrase) {
+    cnt++;
+  }
+  return cnt;
+};
+// console.log(letterCounter());
+
+// sum of array elements
+const sumArray = (number) => {
+  let res = 0;
+  for (const num of number) {
+    res = res + num;
+  }
+  return res;
+};
+const num = [10, 2, 3, 4];
+// console.log(sumArray(num));
+
+// finding max element from array
+const maxElement = (number) => {
+    max = number[0];
+    for(const num of number){
+        if (num > max) {
+            max = num;
+        }
     }
-    return cnt;
-}
-console.log(letterCounter());
+    return max;
+};
+console.log(maxElement(num));
